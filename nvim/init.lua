d484874783da51f0ddc6b9731f793ebcd04387de
Matthew Stortini <matthewstortini@gmail.com>
2026-03-03 11:42:16 -0600

    new dashboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-1>", "<C-w>o")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":b#<CR>")
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
@@ -54,4 +55,8 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {})
+require("lazy").setup("plugins", {
+  rocks = {
+    enabled = false,
+  },
+})
--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,29 +1,29 @@
 return {
   "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 
   config = function()
     local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+    local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
       [[                                                                       ]],
+      [[  ██████   █████                   █████   █████  ███                  ]],
+      [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
+      [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
+      [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
+      [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
+      [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
+      [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
+      [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
       [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", "<cmd>Telescope find_files hidden=true no_ignore=true<CR>"),
+      dashboard.button("e", "  New file", "<cmd>ene <BAR> startinsert <CR>"),
+      dashboard.button("r", "󰄉  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
+      dashboard.button("q", "󰐥  Quit", "<cmd>qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
