badfa4ad5e0fb3ef3679b9e710dd15d7191e44d2
matthewstortini <matthewstortini@gmail.com>
2026-03-03 13:43:23 -0600

    update lsp config, and .gitignore

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,93 +1,110 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-      local mr = require("mason-registry")
+	{
+		"mason-org/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+			local mr = require("mason-registry")
 
-      mr.refresh(function()
-        for _, name in ipairs({
-          "stylua",
-          "clang-format",
-        }) do
-          local ok, pkg = pcall(mr.get_package, name)
-          if ok and not pkg:is_installed() then
-            pkg:install()
-          end
-        end
-      end)
-    end,
-  },
+			mr.refresh(function()
+				for _, name in ipairs({
+					"stylua",
+					"clang-format",
+				}) do
+					local ok, pkg = pcall(mr.get_package, name)
+					if ok and not pkg:is_installed() then
+						pkg:install()
+					end
+				end
+			end)
+		end,
+	},
 
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" },
-      })
-    end,
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd" },
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(ev)
-          local opts = { buffer = ev.buf, silent = true }
+			---------------------------------------------------------------------------
+			-- Keymaps on attach
+			---------------------------------------------------------------------------
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
 
-          -- Info / docs
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
+					-- Info
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
 
-          -- Navigation
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					-- Navigation
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-          -- Actions
-          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+					-- Actions
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
 
-          -- Diagnostics
-          vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+					-- Diagnostics
+					vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-          -- Toggle buffer diagnsotics on/off
-          vim.keymap.set("n", "<leader>td", function()
-            local bufnr = 0
-            local enabled = vim.diagnostic.is_enabled({ bufnr = bufnr })
-            vim.diagnostic.enable(not enabled, { bufnr = bufnr })
-          end, opts)
-        end,
-      })
+					-- Toggle buffer diagnostics on/off
+					vim.keymap.set("n", "<leader>td", function()
+						local bufnr = 0
+						local enabled = vim.diagnostic.is_enabled({ bufnr = bufnr })
+						vim.diagnostic.enable(not enabled, { bufnr = bufnr })
+					end, opts)
+				end,
+			})
 
+			---------------------------------------------------------------------------
+			-- LSP server definitions
+			---------------------------------------------------------------------------
 
-      vim.lsp.config["lua_ls"] = {
-        capabilities = capabilities,
-        cmd = { "lua-language-server" },
-        filetypes = { "lua" },
-        root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-      }
+			vim.lsp.config.lua_ls = {
+				capabilities = capabilities,
+				root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
+					},
+				},
+			}
 
-      vim.lsp.config["clangd"] = {
-        capabilities = capabilities,
-        cmd = { "clangd" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_markers = { "compile_commands.json", "compile_flags.txt", ".git" },
-      }
-    end,
-  },
+			vim.lsp.config.clangd = {
+				capabilities = capabilities,
+				cmd = { "clangd" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				root_markers = {
+					"compile_commands.json",
+					"compile_flags.txt",
+					".clangd",
+					".git",
+				},
+			}
+
+			---------------------------------------------------------------------------
+			-- Enable servers
+			---------------------------------------------------------------------------
+			vim.lsp.enable({ "lua_ls", "clangd" })
+		end,
+	},
 }
