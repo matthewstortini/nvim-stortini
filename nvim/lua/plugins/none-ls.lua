f0a99734d32f0cc87f47f7d95b39894063eda3e8
Matthew Stortini <matthewstortini@gmail.com>
2026-03-15 13:55:16 -0500

    fixing shellcheck error

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"gbprod/none-ls-shellcheck.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -8,11 +11,12 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.shellcheck,
 				null_ls.builtins.formatting.shfmt,
+				require("none-ls-shellcheck.diagnostics"),
+				require("none-ls-shellcheck.code_actions"),
 			},
 		})
 
-		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "lf", vim.lsp.buf.format, {})
 	end,
 }
